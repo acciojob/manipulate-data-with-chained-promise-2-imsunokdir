9f25ae0c4c4f914291271ea287f306b6178ff02e
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-08-23 11:56:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+function updateArr(arr){
+  document.getElementById("output").textContent = arr.join(" ");
+}
+
+function getArray(){
+  return new Promise((resolve)=>{
+    setTimeout(()=>{
+      resolve([1,2,3,4])
+    },3000)
+  })
+}
+
+
+getArray()
+.then((nums)=>{
+  return new Promise((resolve)=>{
+    setTimeout(()=>{
+      const even = nums.filter(i=>i%2==0);
+      updateArr(even);
+      resolve(even);
+    },1000)
+  })
+})
+.then((evenNums)=>{
+  return new Promise((resolve)=>{
+    setTimeout(()=>{
+      const mulEven = evenNums.map(i=>i*2);
+      updateArr(mulEven)
+      resolve(mulEven)
+    },2000)
+  })
+}) 
